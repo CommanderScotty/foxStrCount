e138d952d81253bc2be836b08207e0a9b59736f7
commanderscotty <scott.glaittli@gmail.com>
2021-03-23 17:09:52 -0600

    added docker-compose.yml

--- setup.sh
+++ setup.sh
@@ -1,2 +1,4 @@
 docker build -t foxstrcount .
+#docker volume create hello
+#docker run  -d -v hello:/var/www/html --name foxStrCount -p 8080:80 foxstrcount
 docker run  -d --name foxStrCount -p 8080:80 foxstrcount
