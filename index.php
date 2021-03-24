1c313459661d23f6b4436d3cb587b0143de2d879
commanderscotty <scott.glaittli@gmail.com>
2021-03-23 19:17:41 -0600

    added input capture

--- index.php
+++ index.php
@@ -10,21 +10,23 @@
 
 	<div id='inputArea'>
 		<form method='post'>
-			<label for='strInput'>input</label>
-			<input type='text' id='strInput' placeholder='literally anything you want'/>
-			<label for='excInput'>exclude</label>
-			<input type='text' id='excInput' placeholder='one char plz'/>
-			<input type='submit' name='test' id='test' value='run'/>
+			<input type='text' id='strInput' name='strInput' placeholder='literally anything you want'/>
+			<input type='text' id='excInput' name='excInput' placeholder='one char plz'/>
+			<input type='submit' name='subButton' id='sub'/>
 		</form>
 		<?php
-			function foo() {
-		   		echo "results";
-		   	}
+			function countChars($inString, $inExcept) {
+				echo $inString;
+				echo $inExcept;
+			}
 
-			if(array_key_exists('test',$_POST)) {
-				foo();
-		   	}
+			if (isset($_POST["subButton"])) {
+				$inString = $_POST['strInput'];
+				$inExcept = $_POST['excInput'];
+				countChars($inString, $inExcept);
+			}
 		?>
+
 	</div>
 
 	<div id='outputArea'>
--- setup.sh
+++
@@ -1,4 +0,0 @@
-docker build -t foxstrcount .
-#docker volume create hello
-#docker run  -d -v hello:/var/www/html --name foxStrCount -p 8080:80 foxstrcount
-docker run  -d --name foxStrCount -p 8080:80 foxstrcount
