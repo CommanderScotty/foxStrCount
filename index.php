858bbf78744484d780b05f2ffd5756796db65441
commanderscotty <scott.glaittli@gmail.com>
2021-03-24 19:15:33 -0600

    added hash table

--- index.php
+++ index.php
@@ -16,8 +16,21 @@
 		</form>
 		<?php
 			function countChars($inString, $inExcept) {
-				echo $inString;
-				echo $inExcept;
+				$hashTab = array_fill(0, 128, 0);
+				$inputArr = str_split($inString);
+				$inputExc = str_split($inExcept);
+
+				foreach($inputArr as $item) {
+					++$hashTab[ord($item)];
+				}
+				foreach($inputExc as $item) {
+					$hashTab[ord($item)] = 0;
+				}
+
+				echo "<br>";
+				print_r($hashTab);
+				echo "<br>";
+
 			}
 
 			if (isset($_POST["subButton"])) {
