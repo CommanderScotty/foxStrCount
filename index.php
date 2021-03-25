992a016ccc5bec39c983fc04f7b490e0580ccbd8
commanderscotty <scott.glaittli@gmail.com>
2021-03-25 17:15:39 -0600

    Added visualization

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <head>
 	<title>Character Counter</title>
 	<meta charset='UTF-8'/>
+	<script src="https://cdn.anychart.com/releases/8.9.0/js/anychart-base.min.js" type="text/javascript"></script>
 </head>
 <body>
 	<div id='titlBar'>
@@ -10,9 +11,12 @@
 
 	<div id='inputArea'>
 		<form method='post'>
+			<label for='strInput'>Text</label>
 			<input type='text' id='strInput' name='strInput' placeholder='literally anything you want'/>
+			<br/>
+			<label for='strInput'>Exclude</label>
 			<input type='text' id='excInput' name='excInput' placeholder='one char plz'/>
-			<input type='submit' name='subButton' id='sub'/>
+			<input type='submit' name='subButton' id='sub' value='Submit'/>
 		</form>
 		<?php
 			function countChars($inString, $inExcept) {
@@ -27,9 +31,10 @@
 					$hashTab[ord($item)] = 0;
 				}
 
+				$output = json_encode($hashTab);
+
 				echo "<br>";
-				print_r($hashTab);
-				echo "<br>";
+				echo "<p hidden id='countOutput'>$output</p>";
 
 			}
 
@@ -43,8 +48,44 @@
 	</div>
 
 	<div id='outputArea'>
-		<h2>analysis</h2>
-	</div>
+		<h2>Analysis</h2>
+			<div id="chart">
+			</div>
+			<table id="table">
+				<tr>
+					<th>Character</th>
+					<th>Count</th>
+				</tr>
+			</table>
+			<script>
+				let data = JSON.parse(document.getElementById('countOutput').innerHTML);
+				let toDisplay = [];
+				for (let i = 0; i < data.length; ++i) {
+					if (data[i]) {
+						toDisplay.push({x: String.fromCharCode(i), value: data[i]});
+					}
+				}
+				var chart = anychart.column(toDisplay);
+				chart.tooltip().title("Character Counts");
+				chart.labels(true);
+				chart.container('chart');
+				chart.draw();
 
+				let table = document.getElementById('table');
+				for (let i = 0; i < toDisplay.length; ++i) {
+					let tr = document.createElement('tr');
+					let td0 = document.createElement('td');
+					let td1 = document.createElement('td');
+					
+					td0.innerHTML = toDisplay[i].x;
+					td1.innerHTML = toDisplay[i].value;
+					tr.appendChild(td0);
+					tr.appendChild(td1);
+					table.appendChild(tr);
+				}
+			</script>
+			<div>
+		</div>
+	</div>
 </body>
 </html>
